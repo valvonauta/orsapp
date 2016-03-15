e0b6744fdea681249eeb5edd5933ec27c5675cfb
valvonauta <cittello@gmail.com>
2016-03-15 17:26:55 +0100

    test

--- js/jq.js
+++ js/jq.js
@@ -774,6 +774,7 @@ document.addEventListener('deviceready', function () {
   // Enable to debug issues.
   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   if(app){
+    alert("app");
     var notificationOpenedCallback = function(jsonData) {
       alert('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
     };
